f565f5153d5e698b03d629da5405d5328f879eea
SatsangSeva <satsangsevaonline@gmail.com>
2025-06-24 11:57:00 +0530

    Update admin-routes.js

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -14,6 +14,7 @@ import {
   getCount,
   resetPassword,
   sendWhatsAppOtp,
+  sendNotificationToAllUsers,
   verifyWhatsAppOtp,
 } from "../controllers/admin-controller.js";
 import {
@@ -47,4 +48,5 @@ adminRouter.get("/blog/:id", getBlogById);
 adminRouter.delete("/blog/:id", deleteBlog);
 
 adminRouter.get("/analytics", getCount);
+adminRouter.post('/send-notification' , sendNotificationToAllUsers)
 export default adminRouter;
